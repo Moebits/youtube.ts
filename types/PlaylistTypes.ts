105e7f2651f7ead5a386516e16f0802365c4d1a3
Max Isom <codetheweb@users.noreply.github.com>
2021-04-01 13:54:32 -0500

    Add missing response field to YoutubePlaylistItemsSearch type

--- types/PlaylistTypes.ts
+++ types/PlaylistTypes.ts
@@ -54,6 +54,7 @@ export interface YoutubePlaylistItemsSearch {
         resultsPerPage: number
     },
     items: YoutubePlaylistItem[]
+    nextPageToken: string
 }
 
 export interface YoutubePlaylistItem {
