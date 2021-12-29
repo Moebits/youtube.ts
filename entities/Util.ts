a676320897148587b6555574dcdc1a831242cd67
Samuel Lewis <slewis@atlassian.com>
2021-12-29 10:59:23 +1030

    Remove regex lookbehinds

--- entities/Util.ts
+++ entities/Util.ts
@@ -16,21 +16,21 @@ export class Util {
         let id: string
         if (resolvable.includes("youtube.com") || resolvable.includes("youtu.be")) {
             if (resolvable.includes("channel")) {
-                id = String(resolvable.match(/(?<=channel\/)(.*?)(?=(?:\?|\/|$))/)[0])
+                id = String(resolvable.match(/(channel\/)(.*?)(?=(?:\?|\/|$))/)[2])
             } else if (resolvable.includes("c/")) {
-                const username = String(resolvable.match(/(?<=c\/)(.*?)(?=(?:\?|\/|$))/)[0])
+                const username = String(resolvable.match(/(c\/)(.*?)(?=(?:\?|\/|$))/)[2])
                 id = await this.api.part("search", "id", {q: username, type: "channel"}).then((r) => r.items[0] ? r.items[0].id.channelId : null)
             } else if (resolvable.includes("user/")) {
-                const username = String(resolvable.match(/(?<=user\/)(.*?)(?=(?:\?|\/|$))/)[0])
+                const username = String(resolvable.match(/(user\/)(.*?)(?=(?:\?|\/|$))/)[2])
                 id = await this.api.part("channels", "id", {forUsername: username}).then((r) => r.items[0] ? r.items[0].id : null)
             } else if (resolvable.includes("watch?v=")) {
-                id = String(resolvable.match(/(?<=watch\?v=)(.*?)(?=(?:&|$))/)[0])
+                id = String(resolvable.match(/(watch\?v=)(.*?)(?=(?:&|$))/)[2])
             } else if (resolvable.includes("youtu.be")) {
-                id = String(resolvable.match(/(?<=youtu.be\/)(.*?)(?=(?:\?|$))/)[0])
+                id = String(resolvable.match(/(youtu.be\/)(.*?)(?=(?:\?|$))/)[2])
             } else if (resolvable.includes("playlist?list=")) {
-                id = String(resolvable.match(/(?<=playlist\?list=)(.*?)(?=(?:\?|$))/)[0])
+                id = String(resolvable.match(/(playlist\?list=)(.*?)(?=(?:\?|$))/)[2])
             } else {
-                const query = String(resolvable.match(/(?<=youtube.com\/)(.*?)(?=(?:\?|$))/)[0])
+                const query = String(resolvable.match(/(youtube.com\/)(.*?)(?=(?:\?|$))/)[2])
                 id = await this.api.part("search", "id", {q: query, type: "channel"}).then((r) => r.items[0] ? r.items[0].id.channelId : null)
             }
         } else {
@@ -44,6 +44,8 @@ export class Util {
                 id = resolvable
             }
         }
+
+        console.log({resolvable, id, type});
 
         if (!id) return Promise.reject("Invalid URL or query.")
         return id
