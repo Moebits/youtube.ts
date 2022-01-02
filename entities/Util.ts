9f3210e23abe9afa5c551fff147848996c3189c8
Samuel Lewis <mazuel@gmail.com>
2022-01-02 19:51:03 +1100

    Fix my dumb merge conflict commit and remove console log

--- entities/Util.ts
+++ entities/Util.ts
@@ -45,8 +45,6 @@ export class Util {
             }
         }
 
-        console.log({resolvable, id, type});
-
         if (!id) return Promise.reject("Invalid URL or query.")
         return id
     }
