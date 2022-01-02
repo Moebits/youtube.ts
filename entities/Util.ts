9d8f1369bb104bcb444738d8aa3cbc3203582f00
Samuel Lewis <mazuel@gmail.com>
2022-01-02 19:37:16 +1100

    Switch to replacement on the total match

--- entities/Util.ts
+++ entities/Util.ts
@@ -16,21 +16,21 @@ export class Util {
         let id: string
         if (resolvable.includes("youtube.com") || resolvable.includes("youtu.be")) {
             if (resolvable.includes("channel")) {
-                id = String(resolvable.match(/(channel\/)(.*?)(?=(?:\?|\/|$))/)[2])
+                id = String(resolvable.match(/(channel\/)(.*?)(?=(?:\?|\/|$))/)[0].replace("channel/", ""))
             } else if (resolvable.includes("c/")) {
-                const username = String(resolvable.match(/(c\/)(.*?)(?=(?:\?|\/|$))/)[2])
+                const username = String(resolvable.match(/(c\/)(.*?)(?=(?:\?|\/|$))/)[0].replace("c/", ""))
                 id = await this.api.part("search", "id", {q: username, type: "channel"}).then((r) => r.items[0] ? r.items[0].id.channelId : null)
             } else if (resolvable.includes("user/")) {
-                const username = String(resolvable.match(/(user\/)(.*?)(?=(?:\?|\/|$))/)[2])
+                const username = String(resolvable.match(/(user\/)(.*?)(?=(?:\?|\/|$))/)[0].replace("user/", ""))
                 id = await this.api.part("channels", "id", {forUsername: username}).then((r) => r.items[0] ? r.items[0].id : null)
             } else if (resolvable.includes("watch?v=")) {
-                id = String(resolvable.match(/(watch\?v=)(.*?)(?=(?:&|$))/)[2])
+                id = String(resolvable.match(/(watch\?v=)(.*?)(?=(?:&|$))/)[0].replace("watch?v=", ""))
             } else if (resolvable.includes("youtu.be")) {
-                id = String(resolvable.match(/(youtu.be\/)(.*?)(?=(?:\?|$))/)[2])
+                id = String(resolvable.match(/(youtu.be\/)(.*?)(?=(?:\?|$))/)[0].replace("youtu.be/", ""))
             } else if (resolvable.includes("playlist?list=")) {
-                id = String(resolvable.match(/(playlist\?list=)(.*?)(?=(?:\?|$))/)[2])
+                id = String(resolvable.match(/(playlist\?list=)(.*?)(?=(?:\?|$))/)[0].replace("playlist?list=", ""))
             } else {
-                const query = String(resolvable.match(/(youtube.com\/)(.*?)(?=(?:\?|$))/)[2])
+                const query = String(resolvable.match(/(youtube.com\/)(.*?)(?=(?:\?|$))/)[0].replace("youtube.com/", ""))
                 id = await this.api.part("search", "id", {q: query, type: "channel"}).then((r) => r.items[0] ? r.items[0].id.channelId : null)
             }
         } else {
